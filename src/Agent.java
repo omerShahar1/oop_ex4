603ce4755ec6dd6a826e6c10547f4cc805e7570e
omer <omerzshahar@gmail.com>
2022-01-04 17:00:18 +0200

    start the game class

--- src/Agent.java
+++ src/Agent.java
@@ -1,4 +1,9 @@
+import api.DirectedWeightedGraph;
 import api.GeoLocation;
+import api.Graph;
+import api.NodeData;
+
+import java.util.Iterator;
 
 public class Agent
 {
@@ -8,6 +13,56 @@ public class Agent
     private int dest;
     private double speed;
     private GeoLocation pos;
+    private DirectedWeightedGraph graph;
 
-   // public Agent(int id, double value)
+
+    public Agent(Graph graph, int id, double speed, GeoLocation pos)
+    {
+        this.graph = graph;
+        this.id = id;
+        this.value = 0;
+        this.src = findNode();
+        this.dest = -1;
+        this.speed = speed;
+        this.pos = pos;
+    }
+
+    public int id(){ return this.id;}
+
+    public double value(){ return this.value;}
+
+    public int src(){ return this.src;}
+
+    public int dest(){ return this.dest;}
+
+    public double speed(){ return this.speed;}
+
+    public GeoLocation pos(){ return this.pos;}
+
+    public void setSrc(int newSrc){ this.src = newSrc;}
+
+    public void setDest(int newDest){ this.dest = newDest;}
+
+    public void setPos(GeoLocation newPos){this.pos = newPos;}
+
+    public void setValue(int points){ this.value += points;}
+
+
+    private int findNode()
+    {
+        NodeData temp = null;
+        Iterator<NodeData> nodesIter = graph.nodeIter();
+        while (nodesIter.hasNext())
+        {
+            temp = nodesIter.next();
+            if(temp.getLocation().x() == pos.x() && temp.getLocation().y() == pos.y() && temp.getLocation().z() == pos.z())
+                return temp.getKey();
+        }
+
+        return -1;
+    }
+
+
+
+
 }
---
+++ src/Game.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Game
+{
+    private HashMap<Integer, Agent> agents;
+    private ArrayList<Pokemon> pokemons;
+
+    public Game()
+    {
+        this.agents = new HashMap<>();
+        this.pokemons = new ArrayList<>();
+    }
+
+    public void addPokemon(String jsonStr)
+    {
+
+    }
+
+    public void addAgent(String jsonStr)
+    {
+
+    }
+}
