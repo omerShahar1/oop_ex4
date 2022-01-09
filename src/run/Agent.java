9a96897add9b8c4778381eb3a86cd77204bd8187
oriazadok <oriazadok@gmail.com>
2022-01-09 15:14:47 +0200

    doc2

--- src/run/Agent.java
+++ src/run/Agent.java
@@ -20,38 +20,74 @@ public class Agent {
 
     }
 
+    /**
+     * This function return the id of the agent
+     * @return
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * This function return the value of the agent
+     * @return
+     */
     public double getValue() {
         return value;
     }
 
+    /**
+     * This function get the src of the agent
+     * @return
+     */
     public int getSrc() {
         return src;
     }
 
+    /**
+     * This function set the src of the agent
+     * @return
+     */
     public void setSrc(int src) {
         this.src = src;
     }
 
+    /**
+     * This function get the dest of the agent
+     * @return
+     */
     public int getDest() {
         return dest;
     }
 
+    /**
+     * This function set the dest of the agent
+     * @return
+     */
     public void setDest(int dest) {
         this.dest = dest;
     }
 
+    /**
+     * This function get the speed of the agent
+     * @return
+     */
     public double getSpeed() {
         return speed;
     }
 
+    /**
+     * This function get the position of the agent
+     * @return
+     */
     public GeoLocation getPos() {
         return pos;
     }
 
+    /**
+     * This function set the position of the agent
+     * @return
+     */
     public void setPos(GeoLocation pos) {
         this.pos = pos;
     }
--- src/run/Pokemon.java
+++ src/run/Pokemon.java
@@ -18,22 +18,42 @@ public class Pokemon
         this.pos = pos;
     }
 
+    /**
+     * This function return the value of the pockemon
+     * @return
+     */
     public double getValue() {
         return value;
     }
 
+    /**
+     * This function return the type of the pockemon
+     * @return
+     */
     public int getType() {
         return type;
     }
 
+    /**
+     * This function get the position of the pockemon
+     * @return
+     */
     public GeoLocation getPos() {
         return pos;
     }
 
+    /**
+     * This function get the edge of the pockemon
+     * @return
+     */
     public EdgeData getEdge() {
         return edge;
     }
 
+    /**
+     * This function set the edge of the pockemon
+     * @return
+     */
     public void setEdge(EdgeData edge) {
         this.edge = edge;
     }
